eb378df759aa39b63385097a9a4def4f7f9210e6
Jason Li <jason.li0102@gmail.com>
2017-01-16 18:13:34 -0800

    Added addRoom property to HomepageCtrl

--- app/scripts/controllers/HomepageCtrl.js
+++ app/scripts/controllers/HomepageCtrl.js
@@ -1,6 +1,11 @@
 (function() {
     function HomepageCtrl(Room) {
+        
         this.rooms = Room;
+        
+        this.addRoom = function(newRoomName) {
+            Room.addRoom(newRoomName); //need to figure out how to clear newRoomName
+        };
     }
     
     angular
