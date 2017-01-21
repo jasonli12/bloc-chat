8d373f0922e4cdf08e268c8dd57d68726de83d0b
Jason Li <jason.li0102@gmail.com>
2017-01-20 19:31:20 -0800

    Added functionality to set and store username in cookies

--- app/scripts/controllers/HomepageCtrl.js
+++ app/scripts/controllers/HomepageCtrl.js
@@ -1,18 +1,30 @@
 (function() {
-    function HomepageCtrl(Room, Message) {
-        
+    function HomepageCtrl(Room, Message, BlocChatCookies) {
+
         this.rooms = Room;
-        
+
         this.addRoom = function(newRoomName) {
             Room.addRoom(newRoomName); //need to figure out how to clear newRoomName
         };
-        
+
         this.messages = Message;
-        
+
         this.activeRoomMessages = '';
+
+        this.cookies = BlocChatCookies;
+
+        this.setUsername = function(newUsername) {
+            BlocChatCookies.setUsername(newUsername);
+        };
+
+        this.currentUser = this.cookies.currentUser;
+        this.resetUsernameCookies = this.cookies.resetCookies;
+        // this.resetUsernameCookies('blocChatCurrentUser');
+        console.log(this.currentUser);
+
     }
-    
+
     angular
         .module('blocChat')
-        .controller('HomepageCtrl', ['Room', 'Message', HomepageCtrl]);
+        .controller('HomepageCtrl', ['Room', 'Message','BlocChatCookies', HomepageCtrl]);
 })();
